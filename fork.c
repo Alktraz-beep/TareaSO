d83b729b2773ed526f2603e94a5e060d69cef630
alktraz-beep <josue1999s@hotmail.com>
2020-02-12 19:15:08 -0600

    programa en c completo de Tarea sistemas operativos, con wait y exit

--- fork.c
+++ fork.c
@@ -9,9 +9,38 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/wait.h>
-
+#define MAX 10
 //inicia la funcion que saca la moda de un arreglo
-void moda(){
+int moda(int array[]){
+	//calcula la moda
+	int contador=0,auxiliar[MAX], posicion=0,numero=0,contador2,mayor=0,posicionmayor=0,bandera=0;
+	float suma=0;
+	// se llena el auxiliar con 0
+	for(contador=0;contador<MAX;contador++){
+		auxiliar[contador]=0;
+	}
+	//Recorre el vector comprobando repeticiones
+	//las almacena en el vector auxiliar segun la posicion del numero
+	for(contador=0;contador<MAX;contador++){
+		numero=array[contador];
+		posicion=contador;
+		for(contador2=contador;contador2<MAX;contador2++){
+			if(array[contador2]==numero)
+				auxiliar[posicion]++;
+		}
+	}
+	/* Se establece cual es la posicion del vector que tiene el numero 
+	mayor de repeticiones este corresponde a la moda 
+	*/
+	mayor=auxiliar[0];
+	posicionmayor=0;
+	for(contador=0;contador<MAX;contador++){
+		if(auxiliar[contador]>mayor){
+			posicionmayor=contador;
+			mayor=auxiliar[contador];
+		}
+	}
+	return array[posicionmayor];
 } 
 //funcion para hacer un intercambio en el arreglo
 void cambiar(int *xp, int *yp){
@@ -22,8 +51,8 @@ void cambiar(int *xp, int *yp){
 //funcion para sacar el numero mas grande de un arreglo
 int mayorNumero(int array[]){
 	int i,j;
-	for(i=0;i<10-1;i++){
-		for(j=0;j<10-1;j++){
+	for(i=0;i<MAX-1;i++){
+		for(j=0;j<MAX-1;j++){
 			if(array[j]>array[j+1]){
 				cambiar(&array[j],&array[j+1]);
 			}
@@ -34,17 +63,19 @@ int mayorNumero(int array[]){
 
 void mostrarArreglo(int array[]){
 	int i;
-	for(i=0;i<10;i++){
+	for(i=0;i<MAX;i++){
 		printf("%d-",array[i]);
 	}
 	printf("\n");
 }
+
+
 int main(){
 	int pid;
 	int numero=-1,estado1,pidWait1,numMayor;
 	
 	//--se crea arreglo de 10 numeros
-	int arreglo[10]={60,201,7,1,108,7,50,7,9,7};
+	int arreglo[10]={255,0,7,130,5,130,8,7,130,130};
 
 	pid=fork(); //se crea primer proceso 
 
@@ -86,7 +117,7 @@ int main(){
 	}else{ 
 		//*****si es 0 es el hijo 1
 		printf("ejecutandose proceso hijo 1 moda del arreglo %d...\n",pid);
-		exit(1);
+		exit(moda(arreglo));
 	}
 	return 0;
 }
