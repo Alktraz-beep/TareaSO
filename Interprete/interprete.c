b425bb22d380e1c36d2dfea17ac3e947305de52b
alktraz-beep <josue1999s@hotmail.com>
2020-02-16 00:27:38 -0600

    añadi comentarios

--- Interprete/interprete.c
+++ Interprete/interprete.c
@@ -2,9 +2,11 @@
 #include<stdlib.h>
 #include<string.h>
 int main(){
+	//***crea un apuntador a char dinamico ***/
 	char *comando,*salida="sal";
 	comando=calloc(1024,sizeof(char));
-	
+	//***aqui se lee la cadena que ingresara como comando mientras no sea
+	//*** la palabra "sal",despues imprime la palabra ingresada
 	do{
 		scanf("%s",comando);
 		getchar();
