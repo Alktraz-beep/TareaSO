485a4922c96510635e9bf5c84c82bbb06732ccce
alktraz-beep <josue1999s@hotmail.com>
2020-03-09 19:22:27 -0600

    Programa con dos procesos

--- CuentaBancaria_Peterson/Peterson.c
+++ CuentaBancaria_Peterson/Peterson.c
@@ -1,7 +1,26 @@
+/*
+	Programa Hecho por Josue Yafte Ramírez Viramontes y Juan Pablo Pérez Dublán
+*/
 #include<stdio.h>
+#include<pthread.h>
+int proc_favorecido, proc1deseaEntrar, proc2deseaEntrar;
 //------------proceso 1
+void *deposito_HiloSecundario1(){
+	while(1){
+	}
+}
+
+
 //------------proceso 2
+void *deposito_HiloSecundario2(){
+	while(1){
+	}
+}
 int main(){
 	// aqui va el algoritmo de crear los hios en 0 en paralelo
-	
+	proc1deseaEntrar=0;
+	proc2deseaEntrar=0;
+	int error;
+	pthread_t hilo1,hilo2;
+	return 0;	
 }
